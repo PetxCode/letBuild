1ac885b9d3d7eb41c917afb97a5b84a0a09ad345
codelabite <chukwusahpeter@gmail.com>
2021-02-22 22:30:39 +0100

    fastest

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <h2>Welcome to CI/CD with HEROKU </h2>
         <p>with Peter: CodeLab </p>
         <p>Pushing up to Firebase </p>
+        <p>Which is faster...!</p>
       </header>
     </div>
   );
